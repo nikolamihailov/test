48017b795aea36a1c1929bf9caf95fe35990d3ed
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-08-15 17:03:16 +0300

    app and main refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { RouterProvider } from "react-router-dom";
+import { appRouter } from "./utils/routes";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthProvider } from "./contexts/AuthContext";
+import generateCssVariables from "./utils/ThemeColors/generateColors";
+
+const queryClient = new QueryClient();
+
+const theme = createTheme({
+  typography: {
+    htmlFontSize: 10,
+  },
+  palette: {
+    primary: {
+      main: "#8B0401",
+    },
+    secondary: {
+      main: "#FFF",
+    },
+  },
+});
+
+generateCssVariables(theme);
+
 function App() {
   return (
-    <>
-      <h1>Started!</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <RouterProvider router={appRouter} />
+        </AuthProvider>
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
