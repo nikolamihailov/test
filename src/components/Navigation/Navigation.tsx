c7dd0a6c8440fbec6fa11684a1e203810ad22e83
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-08-16 12:35:29 +0300

    created and used two navs for better code readability

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,17 +2,17 @@ import { useEffect } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { Box } from "@mui/material";
 import { useTheme, useMediaQuery } from "@mui/material";
-import { NavLink, useLocation } from "react-router-dom";
-import Logo from "./Logo/Logo";
+import { useLocation } from "react-router-dom";
 import MobileNav from "./MobileNav/MobileNav";
-import logo from "/logo.png";
 import styles from "./Navigation.module.css";
+import AuthenticatedNav from "./AuthenticatedNav";
+import UnauthenticatedNav from "./UnAuthenticatedNav";
 
 function Navigation() {
   const theme = useTheme();
   const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
   const location = useLocation();
-  const { isAuthenticated, logoutUser } = useAuth();
+  const { isAuthenticated } = useAuth();
 
   useEffect(() => {
     if (location.hash) {
@@ -25,88 +25,14 @@ function Navigation() {
 
   return (
     <>
-      {isMdUp && (
-        <Box component="nav" sx={{ padding: 1.5, bgcolor: theme.palette.primary.main }}>
+      <Box component="nav" sx={{ padding: 1.5, bgcolor: theme.palette.primary.main }}>
+        {isMdUp && (
           <ul className={styles["main-nav"]}>
-            <li>
-              <NavLink to="/#about">About us</NavLink>
-            </li>
-            <li>
-              <NavLink to="/#contact">Contact</NavLink>
-            </li>
-            {isAuthenticated && (
-              <>
-                <li>
-                  <NavLink
-                    to={"/services"}
-                    className={({ isActive }) => (isActive ? styles["active"] : "")}
-                  >
-                    Services
-                  </NavLink>
-                </li>
-              </>
-            )}
-            <li>
-              <NavLink to={"/"}>
-                <Logo src={logo} width={60} />
-              </NavLink>
-            </li>
-            {isAuthenticated && (
-              <>
-                <li>
-                  <NavLink
-                    to={"/appointments"}
-                    className={({ isActive }) => (isActive ? styles["active"] : "")}
-                  >
-                    Appointments
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to={"/profile"}
-                    className={({ isActive }) => (isActive ? styles["active"] : "")}
-                  >
-                    Profile
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to={"/"} onClick={() => logoutUser()}>
-                    Logout
-                  </NavLink>
-                </li>
-              </>
-            )}
-            {!isAuthenticated && (
-              <>
-                <li>
-                  <NavLink
-                    to={"/login"}
-                    className={({ isActive }) => (isActive ? styles["active"] : "")}
-                  >
-                    Login
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to={"/register"}
-                    className={({ isActive }) => (isActive ? styles["active"] : "")}
-                  >
-                    Register
-                  </NavLink>
-                </li>
-              </>
-            )}
+            {isAuthenticated ? <AuthenticatedNav /> : <UnauthenticatedNav />}
           </ul>
-        </Box>
-      )}
-      {!isMdUp && (
-        <Box
-          component="nav"
-          sx={{ padding: ["1.5rem 2.5rem"], bgcolor: theme.palette.primary.main }}
-        >
-          <MobileNav />
-        </Box>
-      )}
+        )}
+        {!isMdUp && <MobileNav />}
+      </Box>
     </>
   );
 }
