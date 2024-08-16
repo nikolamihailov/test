78610ce655dc8ea6dde45aa0cd859e1b6fd09061
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-08-16 12:37:38 +0300

    styles extracted

---
+++ src/utils/StylesHelper/LogoutBtn.ts
@@ -0,0 +1,12 @@
+export const logoutBtnSX = {
+  textDecoration: "none",
+  fontSize: "2rem",
+  fontWeight: 700,
+  backgroundColor: "transparent",
+  textTransform: "none",
+  padding: 0,
+  "&:hover": {
+    backgroundColor: "transparent",
+    color: "#ffe066",
+  },
+};
