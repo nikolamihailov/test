0724566cfde010757420b13d7b07851ecbc834b4
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-08-29 11:36:52 +0300

    styles added and updated

---
+++ src/utils/StylesHelper/Section.ts
@@ -0,0 +1,24 @@
+import { Theme } from "@mui/material";
+import { CSSProperties } from "react";
+
+export const sectionStyles = (theme: Theme): CSSProperties => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "9.6rem 3.2rem",
+  textAlign: "center",
+  gap: "4.8rem",
+  color: theme.palette.primary.main,
+});
+
+export const welcomeSection = (theme: Theme): CSSProperties => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "4.8rem 3.2rem",
+  textAlign: "center",
+  gap: "4.8rem",
+  color: theme.palette.primary.main,
+});
