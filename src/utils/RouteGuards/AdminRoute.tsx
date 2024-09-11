9c0e77b0b9cbd8bf4e05719b01493ce94c3dbf46
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-09-11 14:14:06 +0300

    updated route guards without reduntant use state

--- src/utils/RouteGuards/AdminRoute.tsx
+++ src/utils/RouteGuards/AdminRoute.tsx
@@ -1,52 +1,28 @@
-/* import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import { toast } from "react-toastify";
+import Spinner from "../../components/Spinner/Spinner";
 
 const AdminRoute = () => {
-  const { user } = useAuth();
-  const [shouldRedirect, setShouldRedirect] = useState(false);
-
-  useEffect(() => {
-    if (user?.role && user.role !== "ADMIN") {
-      toast.error("Only admins can access this.");
-      setShouldRedirect(true);
-    }
-  }, [user]);
-
-  if (shouldRedirect) {
-    return <Navigate to="/" replace />;
-  }
-
-  return <Outlet />;
-};
-
-export default AdminRoute;
- */
-import { useEffect, useState } from "react";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
-import { toast } from "react-toastify";
-
-const AdminRoute = () => {
-  const { user, isAuthenticated } = useAuth();
-  const [shouldRedirect, setShouldRedirect] = useState(false);
-  const location = useLocation();
+  const { user, isAuthenticated, isLoading } = useAuth();
 
   useEffect(() => {
     if (!isAuthenticated) {
-      toast.error("Your session has expired. Please log in again.");
-      setShouldRedirect(true);
+      toast.error("Please, log in first to access.");
     } else if (user?.role && user.role !== "ADMIN") {
       toast.error("Only admins can access this.");
-      setShouldRedirect(true);
     }
   }, [isAuthenticated, user]);
 
-  if (shouldRedirect) {
-    return (
-      <Navigate to={!isAuthenticated ? `/login?continue=${location.pathname}` : "/"} replace />
-    );
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (!isAuthenticated) {
+    return <Navigate to={`/login`} replace />;
+  }
+  if (isAuthenticated && user?.role !== "ADMIN") {
+    return <Navigate to={"/home"} />;
   }
 
   return <Outlet />;
